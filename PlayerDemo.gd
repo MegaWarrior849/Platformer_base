bd6ac767e826b6aa439593e54fcab2b7e912b8c3
Caleb Camp <ccamp0105@mymail.lausd.net>
2022-09-21 12:20:21 -0700

    stuff

--- PlayerDemo.gd
+++ PlayerDemo.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-export (int) var speed = 500
-export (int) var jump_speed = -1000
-export (int) var gravity = 2500
+export (int) var speed = 350
+export (int) var jump_speed = -1050
+export (int) var gravity = 2600
 
 export (float, 0, 1.0) var friction = 6
 export (float, 0, 1.0) var acceleration = 6
@@ -13,17 +13,23 @@ func get_input(delta):
 	if Input.is_action_pressed("ui_right"):
 		dir += 1
 		$Sprite.flip_h = true
+		if Input.is_action_pressed("run"):
+			dir += 1
 	if Input.is_action_pressed("ui_left"):
 		dir -= 1
 		$Sprite.flip_h = false
+		if Input.is_action_pressed("run"):
+			dir -= 1
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration * delta)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction * delta)
-	if velocity.y > 0:
-		$Sprite.flip_v = true
+	if Input.is_action_pressed("crouch"):
+		$Sprite.scale.y = 0.138
+		$CollisionShape2D.scale.y = 0.5
 	else:
-		$Sprite.flip_v = false
+		$Sprite.scale.y = 0.276
+		$CollisionShape2D.scale.y = 1
 
 func _physics_process(delta):
 	get_input(delta)
