024ce7a915b6bdaf4635ce669aed27a1dd48b776
Caleb Camp <ccamp0105@mymail.lausd.net>
2022-10-11 11:29:52 -0700

    Ball success

--- LightningThing.gd
+++ LightningThing.gd
@@ -1,4 +1,29 @@
-extends KinematicBody2D
+extends Area2D
+
+export var speed = 0
+export var steer_force = 200.0
 
 
+var velocity = Vector2.ZERO
+var acceleration = Vector2.ZERO
+onready var target = $"../PlayerDemo"
 
+func start(_transform, _target):
+	global_transform = _transform
+	rotation += rand_range(-0.09, 0.09)
+	velocity = transform.x * speed
+	target = _target
+
+func _physics_process(delta):
+	acceleration += seek()
+	velocity += acceleration * delta
+	velocity = velocity.clamped(speed)
+	rotation = velocity.angle()
+	position += velocity * delta
+
+func seek():
+	var steer = Vector2.ZERO
+	if target:
+		var desired = (target.position - position).normalized() * speed
+		steer = (desired - velocity).normalized() * steer_force
+	return steer
--- Main.gd
+++ Main.gd
@@ -5,3 +5,9 @@ func _on_HUD_start_game():
 	$PlayerDemo.gravity = 2600
 	$PlayerDemo.speed = 350
 	$PlayerDemo.jump_speed = -1050
+	$LightningThing.speed = 500
+
+
+func _on_LightningThing_body_entered(body):
+	if body == $PlayerDemo:
+		get_tree().reload_current_scene()
